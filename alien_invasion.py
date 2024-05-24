70efb6cf99faecbde47014f9d0ae023eef4226ec
Eleonora <eleoc4@gmail.com>
2024-05-24 18:10:27 -0500

    Add the settings for bullet an alien collision

--- alien_invasion.py
+++ alien_invasion.py
@@ -117,6 +117,20 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+
+        self._check_bullet_alien_collisions()
+
+
+    def _check_bullet_alien_collisions(self):
+        """Respond to bullet-alien collisions."""
+        # Remove any bullets and aliens that have collided.
+        collisions = pygame.sprite.groupcollide(
+            self.bullets, self.aliens, True, True)
+        
+        if not self.aliens:
+            # Destroy existing bullets and create new fleet.
+            self.bullets.empty()
+            self._create_fleet()
     
     def _update_aliens(self):
         """Check if the fleet is at an edge, then update positions."""
