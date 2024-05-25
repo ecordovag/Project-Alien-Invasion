533e1052313b11fa39643a1623cba3ade85758e1
Eleonora <eleoc4@gmail.com>
2024-05-25 11:47:33 -0500

    Add the final version of exercise 13_5

--- alien_invasion.py
+++ alien_invasion.py
@@ -79,6 +79,19 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.left > 1200:
                 self.bullets.remove(bullet)
+
+        self._check_bullet_alien_collisions()
+    
+    def _check_bullet_alien_collisions(self):
+        """Respond to bullet-alien collisions."""
+        # Remove any bullets and aliens that have collided.
+        collisions = pygame.sprite.groupcollide(
+            self.bullets, self.aliens, True, True)
+        
+        if not self.aliens:
+            # Destroy existing bullets and create new fleet.
+            self.bullets.empty()
+            self._create_fleet()
     
     def _update_aliens(self):
           """Update the positions of all aliens in the fleet."""
--- settings.py
+++ settings.py
@@ -9,14 +9,14 @@ class Settings:
         self.bg_color = (255, 255, 255)
 
         # Ship settings
-        self.ship_speed = 1.5
+        self.ship_speed = 2.5
 
         # Bullet settings
-        self.bullet_speed = 2.0
+        self.bullet_speed = 5.0
         self.bullet_width = 15
         self.bullet_height = 3
         self.bullet_color = (60, 60, 60)
-        self.bullets_allowed = 3
+        self.bullets_allowed = 5
 
         # Alien settings
-        self.alien_speed = 1.0
+        self.alien_speed = 0.5
